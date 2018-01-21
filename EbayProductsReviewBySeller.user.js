c26767c7d2134fe5032dd23fb570fcc8ec73953a
AlekPet <alexepetrof@gmail.com>
2018-01-21 17:54:14 +0300

    license line

--- EbayProductsReviewBySeller.user.js
+++ EbayProductsReviewBySeller.user.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  Ebay check sellers
 // @author       AlekPet 2017
+// @license     MIT;
 // @match        http*://www.ebay.com/*
 // @updateURL    https://github.com/AlekPet/Ebay-User-Reviews-products-by-seller/raw/master/EbayProductsReviewBySeller.user.js  
 // @downloadURL  https://github.com/AlekPet/Ebay-User-Reviews-products-by-seller/raw/master/EbayProductsReviewBySeller.user.js
